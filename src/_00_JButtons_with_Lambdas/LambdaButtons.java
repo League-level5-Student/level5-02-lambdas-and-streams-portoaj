85e4a65dfa009d306ce44780a9c125a398764f25
League Student <league@iMac-34.attlocal.net>
2019-08-07 20:01:04 -0700

    started

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,11 +1,13 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class LambdaButtons {
+public class LambdaButtons{
 	private JFrame window = new JFrame();
 	private JButton addNumbers = new JButton("ADD 2 Numbers");
 	private JButton randNumber = new JButton("RANDOM NUMBER");
@@ -17,10 +19,14 @@ public class LambdaButtons {
 		window.add(addNumbers);
 		window.add(randNumber);
 		window.add(tellAJoke);
-		
+		Random rand = new Random();
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
-		
+		addNumbers.addActionListener((a) -> {
+			int b = 1 + 2;
+			System.out.println("1 + 2 is " + b);
+		});
+		randNumber.addActionListener((a)-> System.out.println());
 		window.setVisible(true);
 		window.pack();
 	}
